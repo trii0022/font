5516112df972eeb78e17dd052d30d94e86d92ae5
Triin Trempi <tri0022@stud.kea.dk>
2022-03-04 15:10:33 +0100

    all the changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "540px",
+      sm: "840px",
+    },
+    extend: {
+      colors: {
+        "footer-blue": "#4BA6A6",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
   plugins: [require("daisyui")],
